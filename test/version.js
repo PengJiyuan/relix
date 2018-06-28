92767a94fb1e2165d0a5fd306d4f9b7d73b88da8
PengJiyuan <731401082@qq.com>
2018-06-28 18:04:31 +0800

    add test case

--- test/version.js
+++ test/version.js
@@ -1,8 +1,23 @@
 const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), -1);
+const semver = require('../semver');
+
+const oldVersion = '1.1.1';
+
+const semverList = [
+  ['patch', true, '1.1.2'],
+  ['minor', true, '1.2.0'],
+  ['major', true, '2.0.0'],
+  ['prepatch', true, '1.1.2-beta.0'],
+  ['preminor', 'alpha', '1.2.0-alpha.0'],
+  ['premajor', 'rc', '2.0.0-rc.0'],
+  ['prerelease', true, '1.1.2-beta.0']
+];
+
+describe('Semver version check (old version: 1.1.1)', function() {
+  semverList.forEach(sem => {
+    const newVersion = semver({[sem[0]]: sem[1]}, oldVersion).version;
+    it(`relix --${sem[0]}${typeof sem[1] === 'string' ? ` ${sem[1]}` : ''} (${oldVersion} -> ${newVersion})`, function() {
+      assert.equal(newVersion, sem[2]);
     });
   });
 });
